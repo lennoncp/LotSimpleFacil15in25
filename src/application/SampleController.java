9a2407bf086ea4c67fc299725e3a3c68e73cbb0d
Lennon <lennon.palmerio@gmail.com>
2020-03-16 22:34:43 -0300

    linhas contadas
    sorteio de quantidades por linha

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -118,6 +118,21 @@ public class SampleController implements Initializable {
     private Button btnSortear;
     
     @FXML
+    private TextField txfC1;
+
+    @FXML
+    private TextField txfC2;
+
+    @FXML
+    private TextField txfC5;
+
+    @FXML
+    private TextField txfC4;
+
+    @FXML
+    private TextField txfC3;
+    
+    @FXML
     private TableView<Aposta> tvApostas;
 
     @FXML
@@ -160,9 +175,17 @@ public class SampleController implements Initializable {
 	    	
 	    	dezenas = new Integer[15];
 	    	
-	    	for(int i = 0; i < 15; i++) {
+	    	/*for(int i = 0; i < 15; i++) {
 	    		dezenas[i] = getSorteador();
-	    	}
+	    	}*/
+	    	int c1 = Integer.valueOf(txfC1.getText());
+	    	int c2 = Integer.valueOf(txfC2.getText());
+	    	int c3 = Integer.valueOf(txfC3.getText());
+	    	int c4 = Integer.valueOf(txfC4.getText());
+	    	int c5 = Integer.valueOf(txfC5.getText());
+	    	
+	    	List<Integer> qtdDezenasPorLinha = Arrays.asList(c1,c2,c3,c4,c5);
+	    	dezenas = getSorteador(qtdDezenasPorLinha);
 	    	
 	    	Integer soma = 0;
 	    	Integer impPar = 0;
@@ -321,6 +344,88 @@ public class SampleController implements Initializable {
     	System.out.print(" ");
     	System.out.println("valor: " + valor  + " index: " + index);
     	return valor;
+    }
+    
+    public Integer[] getSorteador(List<Integer> qtdDezenasPorLinha) {
+    	Integer[] dezenas = new Integer[15];
+    	
+    	//List<Integer> linha1 = Arrays.asList(1,2,3,4,5);
+    	List<Integer> linha1 = new ArrayList<Integer>();
+    	linha1.add(1);
+    	linha1.add(2);
+    	linha1.add(3);
+    	linha1.add(4);
+    	linha1.add(5);
+    	
+    	List<Integer> linha2 = new ArrayList<Integer>();
+    	linha2.add(6);
+    	linha2.add(7);
+    	linha2.add(8);
+    	linha2.add(9);
+    	linha2.add(10);
+    	
+    	List<Integer> linha3 = new ArrayList<Integer>();
+    	linha3.add(11);
+    	linha3.add(12);
+    	linha3.add(13);
+    	linha3.add(14);
+    	linha3.add(15);
+    	
+    	List<Integer> linha4 = new ArrayList<Integer>();
+    	linha4.add(16);
+    	linha4.add(17);
+    	linha4.add(18);
+    	linha4.add(19);
+    	linha4.add(20);
+    	
+    	List<Integer> linha5 = new ArrayList<Integer>();
+    	linha5.add(21);
+    	linha5.add(22);
+    	linha5.add(23);
+    	linha5.add(24);
+    	linha5.add(25);
+    	
+    	int linha = 1;
+    	int indexDezenas = 0;
+    	for(int d : qtdDezenasPorLinha) {
+    		int index = 0;
+    		for(int i = d; i > 0; i--) {
+    			switch (linha) {
+    			case 1:
+    				index = rad.nextInt(linha1.size()); //TODO INDEX COM PROBLEMAS
+    				dezenas[indexDezenas++] = linha1.get(index);
+    				System.out.println("Index: "+index+ " LInha1: "+linha1.toString());
+    				linha1.remove(index);
+    			break;
+    			case 2:
+    				index = rad.nextInt(linha2.size());
+    				dezenas[indexDezenas++] = linha2.get(index);
+    				linha2.remove(index);
+    			break;
+    			case 3:
+    				index = rad.nextInt(linha3.size());
+    				dezenas[indexDezenas++] = linha3.get(index);
+    				linha3.remove(index);
+    			break;
+    			case 4:
+    				index = rad.nextInt(linha4.size());
+    				dezenas[indexDezenas++] = linha4.get(index);
+    				linha4.remove(index);
+    			break;
+    			case 5:
+    				index = rad.nextInt(linha5.size());
+    				dezenas[indexDezenas++] = linha5.get(index);
+    				linha5.remove(index);
+    			break;
+    			default:
+    				break;
+    			}
+    		}
+    		
+    		linha++;
+    	}
+    	
+    	return dezenas;
     }
 
 	@Override
